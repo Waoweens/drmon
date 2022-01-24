39c5892929c73a2f0697232e1a8095a6d80b1575
Waoweens <30313089+Waoweens@users.noreply.github.com>
2022-01-24 14:16:48 +0700

    fix typo

--- drmon.lua
+++ drmon.lua
@@ -175,7 +175,7 @@ function update()
     end
 
     for k, v in pairs (ri) do
-      print(k.. ": ".. v)
+      print(k.. ": "..v)
     end
     print("Output Gate: ", fluxgate.getSignalLowFlow())
     print("Input Gate: ", inputfluxgate.getSignalLowFlow())
